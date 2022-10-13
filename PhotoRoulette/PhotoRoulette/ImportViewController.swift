a608aa4c4900f0eb9f2c473d16af8696f462417f
Yu Jun <yujuny2@github.com>
2022-10-12 21:21:13 -0500

    Added import image feature

---
+++ PhotoRoulette/PhotoRoulette/ImportViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ImportViewController.swift
+//  PhotoRoulette
+//
+//  Created by Yu Jun Yam on 10/12/22.
+//
+
+import UIKit
+
+class ImportViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var PhotoView: UIImageView!
+    
+    @IBAction func ImportButton(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        
+        image.allowsEditing = false
+        
+        self.present(image, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            PhotoView.image = image
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
