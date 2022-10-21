24a11655ee74bd1e443b89cb4fd84387b5994d01
Yu Jun <yujuny2@github.com>
2022-10-21 15:51:37 -0500

    Added GameSelectionViewController and CreateGameViewController

---
+++ PhotoRoulette/PhotoRoulette/GameSelectionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameSelectionViewController.swift
+//  PhotoRoulette
+//
+//  Created by Yu Jun Yam on 10/21/22.
+//
+
+import UIKit
+
+class GameSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // @IBAction func createGame(_ sender: Any) {
+    //     guard let vc = storyboard?.instantiateViewController(identifier: "create_game_vc") as? CreateGameViewController else {
+    //         return
+    //     }
+    //     present(vc, animated: true)
+    // }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
